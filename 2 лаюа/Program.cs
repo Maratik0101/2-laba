0e3572d44307f3fcf1b80cd05d019fc5ee7dbce1
админ <@DESKTOP-MDU52T5>
2023-11-14 12:17:33 +0300

    Добавьте файлы проекта.

---
+++ 2 лаюа/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите значение a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение c");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a + b == 0 || b + c == 0 || a + c == 0)
+{
+    Console.WriteLine("Одна из пар взаимно противоположна ");
+}
+else
+{
+    Console.WriteLine("Они взаимно не противоположны");
+}
